0d06d800a35cabff9c6b6fd721c455170814f388
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-12-16 20:41:49 +0600

    Создал хедер и футер и примерно сверстал главную страницу

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
+import Blog from "./Container/Blog/Blog";
 
 class App extends Component{
   render() {
     return(
         <div className="App">
-
+            <Blog/>
         </div>
     )
   }
